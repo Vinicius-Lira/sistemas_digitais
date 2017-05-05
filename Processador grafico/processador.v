9f5d1b747a2f8a9b44dae58fc853fb490ca05e84
Vinicius-Lira <viniciusklira@gmail.com>
2017-05-05 18:34:58 -0300

    Processador gráfico

--- Processador grafico/processador.v
+++ Processador grafico/processador.v
@@ -26,13 +26,14 @@ parameter vfp = 511; 	// Início da varanda frontal vertical
 reg [9:0] hc;
 reg [9:0] vc;
 
-// Horizontal & vertical counters --
-// this is how we keep track of where we are on the screen.
+// Contadores Horizontais e verticais
+// Esta é a forma como manter o controle de onde estamos na tela.
 // ------------------------
-// Sequential "always block", which is a block that is
-// only triggered on signal transitions or "edges".
-// posedge = rising edge  &  negedge = falling edge
-// Assignment statements can only be used on type "reg" and need to be of the "non-blocking" type: <=
+// Seqüencial "always block", que é um bloco que é
+// somente acionado em transições de sinal ou "bordas".
+// posedge = borda ascendente & negedge = borda descendente
+// As instruções de atribuição só podem ser usadas no tipo "reg" e precisam ser do tipo "não bloqueador": <=
+
 always @(posedge dclk or posedge clr)
 begin
 	// reset condition
@@ -43,14 +44,14 @@ begin
 	end
 	else
 	begin
-		// keep counting until the end of the line
+		// continua contando até o final da linha
 		if (hc < hpixels - 1)
 			hc <= hc + 1;
 		else
-		// When we hit the end of the line, reset the horizontal
-		// counter and increment the vertical counter.
-		// If vertical counter is at the end of the frame, then
-		// reset that one too.
+		// Quando atingimos o fim da linha, restabelece a posição horizontal
+		// counter e incrementar o contador vertical.
+		// Se o contador vertical estiver no final do frame, então
+		// reset que um também.
 		begin
 			hc <= 0;
 			if (vc < vlines - 1)
@@ -62,86 +63,87 @@ begin
 	end
 end
 
-// generate sync pulses (active low)
+// geram pulsos de sincronização (baixa ativa)
 // ----------------
-// "assign" statements are a quick way to
-// give values to variables of type: wire
+// "assign" declarações são uma maneira rápida de
+// dá valores a variáveis ​​de tipo: wire
 assign hsync = (hc < hpulse) ? 0:1;
 assign vsync = (vc < vpulse) ? 0:1;
 
-// display 100% saturation colorbars
+
+// exibir barras de cores de saturação de 100%
 // ------------------------
-// Combinational "always block", which is a block that is
-// triggered when anything in the "sensitivity list" changes.
-// The asterisk implies that everything that is capable of triggering the block
-// is automatically included in the sensitivty list.  In this case, it would be
-// equivalent to the following: always @(hc, vc)
-// Assignment statements can only be used on type "reg" and should be of the "blocking" type: =
+// Combinacional "always block", que é um bloco que é
+// disparado quando algo na "lista de sensibilidade" muda.
+// O asterisco implica que tudo o que é capaz de acionar o bloco
+// é automaticamente incluído na lista de sensibilidade. Neste caso, seria
+// equivalente ao seguinte: always @ (hc, vc)
+// As instruções de atribuição só podem ser usadas no tipo "reg" e devem ser do tipo "bloqueio": =
 always @(*)
 begin
-	// first check if we're within vertical active video range
+	// primeiro verifique se estamos dentro do intervalo de vídeo ativo vertical
 	if (vc >= vbp && vc < vfp)
 	begin
-		// now display different colors every 80 pixels
-		// while we're within the active horizontal range
-		// -----------------
-		// display white bar
+	// agora exibem cores diferentes a cada 80 pixels
+	// enquanto estamos dentro da faixa horizontal ativa
+	// -----------------
+	// exibir barra branca
 		if (hc >= hbp && hc < (hbp+80))
 		begin
 			red = 3'b111;
 			green = 3'b111;
 			blue = 2'b11;
 		end
-		// display yellow bar
+		// exibir barra amarela
 		else if (hc >= (hbp+80) && hc < (hbp+160))
 		begin
 			red = 3'b111;
 			green = 3'b111;
 			blue = 2'b00;
 		end
-		// display cyan bar
+		// exibir barra ciano
 		else if (hc >= (hbp+160) && hc < (hbp+240))
 		begin
 			red = 3'b000;
 			green = 3'b111;
 			blue = 2'b11;
 		end
-		// display green bar
+		// exibir barra verde
 		else if (hc >= (hbp+240) && hc < (hbp+320))
 		begin
 			red = 3'b000;
 			green = 3'b111;
 			blue = 2'b00;
 		end
-		// display magenta bar
+		//exibir barra magenta
 		else if (hc >= (hbp+320) && hc < (hbp+400))
 		begin
 			red = 3'b111;
 			green = 3'b000;
 			blue = 2'b11;
 		end
-		// display red bar
+		//exibir barra vermenha
 		else if (hc >= (hbp+400) && hc < (hbp+480))
 		begin
 			red = 3'b111;
 			green = 3'b000;
 			blue = 2'b00;
 		end
-		// display blue bar
+		// Exibir barra azul
 		else if (hc >= (hbp+480) && hc < (hbp+560))
 		begin
 			red = 3'b000;
 			green = 3'b000;
 			blue = 2'b11;
 		end
-		// display black bar
+		// exibir barra preta
 		else if (hc >= (hbp+560) && hc < (hbp+640))
 		begin
 			red = 3'b000;
 			green = 3'b000;
 			blue = 2'b00;
 		end
-		// we're outside active horizontal range so display black
+		// estamos fora da faixa horizontal ativa para exibir preto
 		else
 		begin
 			red = 0;
@@ -149,7 +151,7 @@ begin
 			blue = 0;
 		end
 	end
-	// we're outside active vertical range so display black
+	// estamos fora da faixa vertical ativa para exibir preto
 	else
 	begin
 		red = 0;
