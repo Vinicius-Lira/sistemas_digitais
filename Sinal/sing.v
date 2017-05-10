f632fa85b99aba72604625803f340b465301448e
Vinicius-Lira <viniciusklira@gmail.com>
2017-05-09 21:05:48 -0300

    Sistemas digitais

--- Sinal/sing.v
+++ Sinal/sing.v
@@ -14,6 +14,17 @@ module sing(
       reg [9:0] ptx;
       reg [9:0] pty;
       reg [2:0] state = 1'b0;
+      wire [9:0] sing;
+      wire [10:0] p1;
+      wire [10:0] p2;
+      wire [10:0] p3;
+      wire [10:0] p4;
+
+      assign p1 = p1x - p3x;
+      assign p2 = p2y - p3y;
+      assign p3 = p2x - p3x;
+      assign p4 = p1y - p3y;
+      assign sing = p1 * p2 - p3 * p4;
 
       always @(selPonto) begin
               if (state == 1'b0) begin
@@ -37,4 +48,30 @@ module sing(
               end
       end
 
+endmodule
+
+
+module test();
+        reg [9:0] px;
+        reg [9:0] py;
+        reg selPonto;
+        wire led;
+
+        sing sinal(px, py, selPonto, led);
+
+        initial begin
+                $dumpvars(0, sinal);
+                #2 selPonto = 0;
+                #2
+                px <= 2; py <= 2; selPonto = 1;
+                #2 selPonto = 0;
+                #2
+                px <= 0; py <= 0; selPonto = 1;
+                #2 selPonto = 0;
+                #2
+                px <= 4; py <= 0; selPonto = 1;
+                #2 selPonto = 0;
+                #2
+                px <= 3; py <= 1; selPonto = 1;
+        end
 endmodule
