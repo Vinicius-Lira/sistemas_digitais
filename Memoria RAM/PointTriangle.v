3575adab78fda2dedf1d8b3f353a2fe8ef51e017
Vinicius-Lira <viniciusklira@gmail.com>
2017-06-06 22:14:48 -0300

    Update PointTriangle.v

--- Memoria RAM/PointTriangle.v
+++ Memoria RAM/PointTriangle.v
@@ -1,22 +1,22 @@
 module PointTriangle(
     input CLOCK_50,
 
-    input [11:0] PointPx,
-    input [11:0] PointPy,
+    input [10:0] PointPx,
+    input [10:0] PointPy,
 
-    input [11:0] PointAx,
-    input [11:0] PointAy,
+    input [10:0] PointAx,
+    input [10:0] PointAy,
 
-    input [11:0] PointBx,
-    input [11:0] PointBy,
+    input [10:0] PointBx,
+    input [10:0] PointBy,
 
-    input [11:0] PointCx,
-    input [11:0] PointCy
+    input [10:0] PointCx,
+    input [10:0] PointCy
 
 );
 
-    reg [11:0] x;
-    reg [11:0] y;
+    reg [10:0] x;
+    reg [10:0] y;
 
     reg enable = 1;
 
@@ -28,21 +28,22 @@ module PointTriangle(
     reg auxRigth = 0;
     reg auxTopY = 0;
 
-    reg [31:0] PointMen;
+    reg [21:0] PointX;
+	reg [8:0] PointTopY;
 
     always @(*) begin
         while(enable) begin
             if(inTriangle) begin
                 if(auxLeftX == 0) begin
-                    PointMen[11:0] <= x;
+                    PointX[10:0] <= x;
                     auxLeftX <= 1;
                 end
                 else begin
-                    PointMen[21:12] <= x;
+                    PointX[21:11] <= x;
                 end
 
                 if(auxTopY == 0) begin
-                    PointMen[31:22] <= y;
+                    PointTopY[8:0] <= y;
                     auxTopY <= 1;
                 end
             end
@@ -132,5 +133,72 @@ module PointInTriangle(
     sing S3(PTX, PTY, P3X, P3Y, P1X, P1Y, sin3);
 
     assign inTriangle = (sin1 == sin2 && sin2 == sin3) ? 1 : 0;
+
+endmodule
+
+module MemoriaRAM(
+    input CLOCK_50,
+    output [7:0] LEDG,//leds
+    output [7:0] LEDR,//leds
+    output [17:0] SRAM_ADDR,//Endereço memoria onde vai ser gravado o SRAM_DQ
+    inout [15:0] SRAM_DQ,//Valor a ser passado para a memoria
+    output SRAM_WE_N,//Sinal para gravar na memoria (write)
+    output SRAM_OE_N,//Sinal para ler a memoria(read)
+    output SRAM_UB_N,
+    output SRAM_LB_N,
+    output SRAM_CE_N
+);
+
+	wire [15:0] output_leds;
+
+	assign LEDR[7:0] = output_leds[7:0];
+	assign LEDG[7:0] = output_leds[7:0];
+
+	assign output_leds = SRAM_DQ;
+	
+	assign SRAM_CE_N = 0;
+
+	reg [3:0] state;
+
+
+	reg [17:0] addr_reg;
+	reg [15:0] data_reg;
+
+	assign SRAM_ADDR = addr_reg;
+	assign SRAM_DQ = data_reg;
+
+
+	reg we, oe, ub, lb; //, ce;
+
+	assign SRAM_WE_N = we;
+	assign SRAM_OE_N = oe;
+	assign SRAM_UB_N = ub;
+	assign SRAM_LB_N = lb;
+
+
+	always @(posedge CLOCK_50) begin
+
+		case (state)
+			0: begin
+				state <= 1;
+				addr_reg <= 13;
+				data_reg <= 2;
+				we <= 0;
+				oe <= 1;
+				ub <= 0;
+				lb <= 0;
+			end
+			2: begin
+				state <= 0;
+				addr_reg <= 13;
+				data_reg <= 16'bzzzzzzzzzzzzzzzz;
+				we <= 0;
+				oe <= 1;
+				ub <= 0;
+				lb <= 0;
+			end
+		endcase
+
+	end
 
 endmodule
